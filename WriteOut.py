8fd9906366b537c17099f1fb0c79a4f5ea502e42
Joseph Edmiston <joseph.edmiston@gmail.com>
2021-02-05 01:33:22 -0500

    updated write out

---
+++ WriteOut.py
@@ -0,0 +1,19 @@
+import csv
+from os.path import join, abspath
+from mailmerge import MailMerge
+
+def write_out(map, responsesFilePath, template, folder):
+    with open(responsesFilePath, encoding='utf8', newline='') as auditionsFile:
+        auditions = csv.DictReader(auditionsFile)
+
+        next(auditions)
+        for audition in auditions:
+            document = MailMerge(template)
+            for field in document.get_merge_fields():
+                document.merge( field = audition[map[field]] )
+
+            filename = str(audition["Child's Name"]).strip()+".docx"
+            filepath = abspath(join(folder, filename))
+
+            document.write(filepath)
+            document.close()
